0fafdcb01b5de5659d032bb0aad3d322834f6595
Fakhrul Islam <fakhrulif@gmail.com>
2023-04-14 01:26:38 +0600

    Asha kori kaj hoiseeeee! Changes to be committed: 	modified:   index.js 	modified:   package-lock.json 	modified:   package.json 	modified:   yarn.lock

--- index.js
+++ index.js
@@ -3,12 +3,27 @@ const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
-
+const jwt = require("jsonwebtoken");
 const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
-
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  console.log(authHeader, req.headers.authorization);
+  if (!authHeader) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    console.log("decoded", decoded);
+    req.decoded = decoded;
+    next();
+  });
+}
 const uri = `${process.env.MONGO_URI}`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -29,129 +44,92 @@ const run = async () => {
       products = await cursor.toArray();
       res.send(products);
     });
-    app.post("/user", async (req, res) => {
+
+    //finding my items thorugh my email address
+
+    // AUTH
+    app.post("/login", async (req, res) => {
       const user = req.body;
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1d",
+      });
+      res.send({ accessToken });
+    });
 
-      const result = await userCollection.insertOne(user);
+    app.get("/myitems", verifyJWT, async (req, res) => {
+      const decodedEmail = req.decoded.email;
+      const email = req.query.email;
+      console.log(decodedEmail, email);
+      if (email === decodedEmail) {
+        const quantity = req.body;
+        const query = { email: email };
+        const cursor = data.find(query);
+        const orders = await cursor.toArray();
+        res.send(orders);
+      } else {
+        res.status(403).send({ message: "forbidden access" });
+      }
+    });
 
+    // single product
+    app.get(`/inventory/:id`, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await data.findOne(query);
+      res.send(product);
+    });
+
+    //delete product
+
+    app.delete(`/inventory/:id`, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await data.deleteOne(query);
       res.send(result);
     });
 
-    app.get("/user/:email", async (req, res) => {
-      const email = req.params.email;
-
-      const result = await userCollection.findOne({ email });
-
-      if (result?.email) {
-        return res.send({ status: true, data: result });
-      }
-
-      res.send({ status: false });
+    //adding product
+    app.post("/inventory", async (req, res) => {
+      const newProduct = req.body;
+      console.log(`newProduct added ${newProduct}`);
+      const product = await data.insertOne(newProduct);
+      res.send(product);
     });
 
-    app.patch("/apply", async (req, res) => {
-      const userId = req.body.userId;
-      const jobId = req.body.jobId;
-      const email = req.body.email;
-
-      const filter = { _id: ObjectId(jobId) };
-      const updateDoc = {
-        $push: { applicants: { id: ObjectId(userId), email } },
-      };
-
-      const result = await jobCollection.updateOne(filter, updateDoc);
-
-      if (result.acknowledged) {
-        return res.send({ status: true, data: result });
-      }
-
-      res.send({ status: false });
-    });
-
-    app.patch("/query", async (req, res) => {
-      const userId = req.body.userId;
-      const jobId = req.body.jobId;
-      const email = req.body.email;
-      const question = req.body.question;
-
-      const filter = { _id: ObjectId(jobId) };
-      const updateDoc = {
-        $push: {
-          queries: {
-            id: ObjectId(userId),
-            email,
-            question: question,
-            reply: [],
-          },
-        },
-      };
-
-      const result = await jobCollection.updateOne(filter, updateDoc);
-
-      if (result?.acknowledged) {
-        return res.send({ status: true, data: result });
-      }
-
-      res.send({ status: false });
-    });
-
-    app.patch("/reply", async (req, res) => {
-      const userId = req.body.userId;
-      const reply = req.body.reply;
-      console.log(reply);
-      console.log(userId);
-
-      const filter = { "queries.id": ObjectId(userId) };
-
-      const updateDoc = {
-        $push: {
-          "queries.$[user].reply": reply,
-        },
-      };
-      const arrayFilter = {
-        arrayFilters: [{ "user.id": ObjectId(userId) }],
-      };
-
-      const result = await jobCollection.updateOne(
-        filter,
-        updateDoc,
-        arrayFilter
-      );
-      if (result.acknowledged) {
-        return res.send({ status: true, data: result });
-      }
-
-      res.send({ status: false });
-    });
-
-    app.get("/applied-jobs/:email", async (req, res) => {
-      const email = req.params.email;
-      const query = { applicants: { $elemMatch: { email: email } } };
-      const cursor = jobCollection.find(query).project({ applicants: 0 });
-      const result = await cursor.toArray();
-
-      res.send({ status: true, data: result });
-    });
-
-    app.get("/jobs", async (req, res) => {
-      const cursor = jobCollection.find({});
-      const result = await cursor.toArray();
-      res.send({ status: true, data: result });
-    });
-
-    app.get("/job/:id", async (req, res) => {
+    // add+ deliver product
+    app.put(`/inventory/:id`, async (req, res) => {
       const id = req.params.id;
-
-      const result = await jobCollection.findOne({ _id: ObjectId(id) });
-      res.send({ status: true, data: result });
+      const updatedProduct = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updatedDoc = {
+        $set: {
+          quantity: updatedProduct.quantity,
+        },
+      };
+      const result = await data.updateOne(filter, updatedDoc, options);
+      res.send(result);
     });
 
-    app.post("/job", async (req, res) => {
-      const job = req.body;
-
-      const result = await jobCollection.insertOne(job);
-
-      res.send({ status: true, data: result });
+    app.put(`/editproduct/:id`, async (req, res) => {
+      const id = req.params.id;
+      const updatedProduct = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updatedDoc = {
+        $set: {
+          quantity: updatedProduct.quantity,
+          description: updatedProduct.description,
+          price: updatedProduct.price,
+          email: updatedProduct.email,
+          url: updatedProduct.url,
+          supplier_name: updatedProduct.supplier_name,
+          price: updatedProduct.price,
+          name: updatedProduct.name,
+        },
+      };
+      const result = await data.updateOne(filter, updatedDoc, options);
+      res.send(result);
     });
   } finally {
   }
@@ -160,9 +138,9 @@ const run = async () => {
 run().catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello Sirr Ovi...!!!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`http://localhost:${port}`);
 });
